e2026f3b61f6394b579078fec76bd65387e8c39d
patOS <patosorio.88@gmail.com>
2024-01-21 18:21:56 +0100

    Connected firebase

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import os
+import firebase_admin
 from flask import Flask, render_template
+from firebase_admin import credentials, firestore
 if os.path.exists("env.py"):
     import env
 
+# Create Flask app
 app = Flask(__name__)
 
+# Connect to Firestore DB
+json_file_path = os.environ.get("FIREBASE_CREDENTIALS_PATH")
+cred = credentials.Certificate(json_file_path)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+db.collection("foods").add(
+    {"img_url": "", 
+     "is_processed":False})
 
 @app.route("/")
 def hello_world():
